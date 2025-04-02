a95903aa8502459d892162d64aa3ae2af03db631
Rafael Conse <solodineroypoder@gmail.com>
2025-04-01 22:12:11 -0600

    fixed overfowX component Habilidades

--- src/components/habilidades/Habilidades.jsx
+++ src/components/habilidades/Habilidades.jsx
@@ -15,74 +15,95 @@ import { motion } from "framer-motion";
 export const Habilidades = () => {
   return (
     <section
-      className="container habilidades h-[50vh] sm:h-[100vh] md:my-36 flex flex-col "
+      className="container my-20 h-[50vh]  md:my-36 flex flex-col "
       id="habilidadesDiv"
     >
-      <div className="w-screen min-h-[100px] sm:min-h-[200px]  flex justify-center items-center relative overflow-hidden ">
-        <motion.div
-          className="w-[33%] sm:flex-1  h-[80%] sm:h-[50%] bg-blue-400 z-10 rounded-sm"
-          initial={{ y: 0 }}
-          whileInView={{ y: "100%", transition: { delay: 0.5, duration: 0.6 } }}
-        ></motion.div>
-        <motion.div
-          className="w-[33%] sm:flex-1  h-[80%] sm:h-[50%] bg-blue-400 z-10 rounded-sm"
-          initial={{ y: 0 }}
-          whileInView={{ y: "-90%", transition: { delay: 0.5, duration: 0.6 } }}
-        ></motion.div>
-        <motion.div
-          className="w-[33%] sm:flex-1  h-[80%] sm:h-[50%] bg-blue-400 z-10 rounded-sm"
-          initial={{ y: 0 }}
-          whileInView={{ y: "100%", transition: { delay: 0.5, duration: 0.6 } }}
-        ></motion.div>
-        <h2 className="text-4xl sm:text-8xl font-medium absolute">
-          {"<Skills />"}
-        </h2>
+      <div className="text-center w-full h-30">
+        <h2 className="text-4xl sm:text-8xl font-medium ">Skills</h2>
       </div>
 
-      <div className="bg-slate-800  p-5 md:p-2 mt-10 rounded-md text-white  shadow-[-16px_41px_32px_10px_#00000024]">
+      <div className="bg-slate-800 w-[95%] px-2 py-4 md:p-2 mt-10 rounded-md text-white  shadow-[-16px_41px_32px_10px_#00000024]">
         <div className="card-body habilidades-tecnologias">
-          <div className="contenido-habilidades w-screen sm:w-auto">
-            <div className="skill ">
-              <img src={javascript} alt="imagen Javascript" />
-
-              <p>JAVASCRIPT</p>
-            </div>
-            <div className="skill ">
-              <img src={css3} alt="imagen Css3" />
-
-              <p>CSS3</p>
-            </div>
-            <div className="skill">
-              <img src={html} alt="imagen HTML" />
-
-              <p>HTML</p>
-            </div>
-            <div className="skill">
-              <img src={git} alt="imagen Git" />
-              <p>GIT</p>
-            </div>
-            <div className="skill">
-              <img src={node} alt="imagen nodejs" />
-              <p>NODE JS</p>
-            </div>
-            <div className="skill">
-              <img src={react} alt="imagen reactjs" />
-
-              <p>REACT JS</p>
-            </div>
-            <div className="skill">
-              <img src={python} alt="imagen Python" />
-
-              <p>PYTHON</p>
+          <div className="flex flex-wrap  sm:w-auto gap-4 justify-center w-full">
+            <div className="flex flex-col items-center gap-2">
+              <img
+                src={javascript}
+                alt="imagen Javascript"
+                className="w-14 h-14 sm:w-28 sm:h-28 xl:w-32 xl:h-32 2xl:w-42 2xl:h-42"
+              />
+              <p className="text-xs sm:text-lg  text-center">JAVASCRIPT</p>
             </div>
 
-            <div className="skill">
-              <img src={electron} alt="imagen Electronjs" />
-              <p>ELECTRON JS</p>
+            <div className="flex flex-col items-center gap-2">
+              <img
+                src={css3}
+                alt="imagen Css3"
+                className="w-14 h-14 sm:w-28 sm:h-28 xl:w-32 xl:h-32 2xl:w-42 2xl:h-42"
+              />
+              <p className="text-xs sm:text-lg  text-center">CSS3</p>
             </div>
-            <div className="skill">
-              <img src={tailwind} alt="imagen Tailwind" />
-              <p>TAILWIND CSS</p>
+
+            <div className="flex flex-col items-center gap-2">
+              <img
+                src={html}
+                alt="imagen HTML"
+                className="w-14 h-14 sm:w-28 sm:h-28 xl:w-32 xl:h-32 2xl:w-42 2xl:h-42"
+              />
+              <p className="text-xs sm:text-lg  text-center">HTML</p>
+            </div>
+
+            <div className="flex flex-col items-center gap-2">
+              <img
+                src={git}
+                alt="imagen Git"
+                className="w-14 h-14 sm:w-28 sm:h-28 xl:w-32 xl:h-32 2xl:w-42 2xl:h-42"
+              />
+              <p className="text-xs sm:text-lg  text-center">GIT</p>
+            </div>
+
+            <div className="flex flex-col items-center gap-2">
+              <img
+                src={node}
+                alt="imagen Nodejs"
+                className="w-14 h-14 sm:w-28 sm:h-28 xl:w-32 xl:h-32 2xl:w-42 2xl:h-42"
+              />
+              <p className="text-xs sm:text-lg  text-center">NODE JS</p>
+            </div>
+
+            <div className="flex flex-col items-center gap-2">
+              <img
+                src={react}
+                alt="imagen Reactjs"
+                className="w-14 h-14 sm:w-28 sm:h-28 xl:w-32 xl:h-32 2xl:w-42 2xl:h-42"
+              />
+              <p className="text-xs sm:text-lg  text-center">REACT JS</p>
+            </div>
+
+            <div className="flex flex-col items-center gap-2">
+              <img
+                src={python}
+                alt="imagen Python"
+                className="w-14 h-14 sm:w-28 sm:h-28 xl:w-32 xl:h-32 2xl:w-42 2xl:h-42"
+              />
+              <p className="text-xs sm:text-lg  text-center">PYTHON</p>
+            </div>
+
+            <div className="flex flex-col items-center gap-2">
+              <img
+                src={electron}
+                alt="imagen Electronjs"
+                className="w-14 h-14 sm:w-28 sm:h-28 xl:w-32 xl:h-32 2xl:w-42 2xl:h-42"
+              />
+              <p className="text-xs sm:text-lg  text-center">ELECTRON JS</p>
+            </div>
+
+            <div className="flex flex-col items-center gap-2">
+              <img
+                src={tailwind}
+                alt="imagen Tailwind"
+                className="w-14 h-14 sm:w-28 sm:h-28 xl:w-32 xl:h-32 2xl:w-42 2xl:h-42"
+              />
+              <p className="text-xs sm:text-lg  text-center">TAILWIND CSS</p>
             </div>
           </div>
         </div>
